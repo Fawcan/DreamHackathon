10eb827677568f126907f01265fa91947fe6399e
Gustaf Wall <wall.gustaf@gmail.com>
2016-11-25 16:52:36 +0100

    Created color manager script and a script with a function for combining colors

---
+++ DreamHackathon/Assets/Scripts/ColorCombination.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCombination : MonoBehaviour
+{
+    [SerializeField] private Color firstColor;
+    [SerializeField] private Color secondColor;
+    [SerializeField] private Color newColor;
+
+
+
+    public void Combine(GameObject objOne, GameObject objTwo)
+    {
+        if (objOne.GetComponent<ColorManager>() != null &&
+            objTwo.GetComponent<ColorManager>() != null)
+        {
+            firstColor = objOne.GetComponent<ColorManager>().Color;
+            secondColor = objTwo.GetComponent<ColorManager>().Color;
+
+            newColor = ColorCombine(firstColor, secondColor);
+
+            objOne.GetComponent<ColorManager>().NewColor = newColor;
+
+            Destroy(objTwo);
+
+        }
+    }
+
+
+    Color ColorCombine(Color firstColor, Color secondColor)
+    {
+        if (this.firstColor == secondColor)
+        {
+            return firstColor;
+        }
+
+        else if (firstColor == Color.red && secondColor == Color.yellow
+            || firstColor == Color.yellow && secondColor == Color.red)
+        {
+            newColor = new Color(255, 165, 0);
+        }
+
+        else if (firstColor == Color.yellow && secondColor == Color.blue
+            || firstColor == Color.blue && secondColor == Color.yellow)
+        {
+            newColor = Color.green;
+        }
+
+        else if (firstColor == Color.red && secondColor == Color.blue 
+            || firstColor == Color.blue && secondColor == Color.red)
+        {
+            newColor = new Color(128, 0, 128);
+        }
+
+
+
+            return newColor;
+    }
+}
--- DreamHackathon/Assets/Scripts/ColorManager.cs
+++ DreamHackathon/Assets/Scripts/ColorManager.cs
@@ -3,7 +3,33 @@ using System.Collections;
 
 public class ColorManager : MonoBehaviour
 {
-    [SerializeField]
-    private Color color;
+    [SerializeField] private Color currentColor;
+    [SerializeField] private Color newColor;
+
+    public Color NewColor
+    {
+        set { newColor = value; }
+    }
+
+    public Color Color
+    {
+        get { return currentColor; }
+    }
+
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = currentColor;
+    }
+
+
+    void Update()
+    {
+        if (currentColor != newColor)
+        {
+            gameObject.GetComponent<Renderer>().material.color = newColor;
+            currentColor = newColor;
+        }   
+    }
 
 }

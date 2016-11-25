56ff308fe494338a4dce39b6cc81367be15592d9
Gustaf Wall <wall.gustaf@gmail.com>
2016-11-25 15:23:14 +0100

    Created folder structure and added DestroyOverTime and DontDestroyOnLoad scripts

---
+++ DreamHackathon/Assets/Scripts/General Scripts/DestroyOverTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    [SerializeField] private float timer = 0;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(timer >= cooldown)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            timer += Time.deltaTime;
+        }
+	}
+}

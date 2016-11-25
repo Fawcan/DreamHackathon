f731d41b7d725856b7218d9b192f9159b9bc0096
UTB\frisal <albinfrisk@hotmail.com>
2016-11-25 23:13:34 +0100

    Fixed everything, i promise

---
+++ ViveSandboxProj/Assets/Scripts/General Scripts/DestroyOverTime.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    [SerializeField] private float timer = 0;
+    private bool countDownFinished = false;
+
+	// Update is called once per frame
+    void Start()
+    {
+        timer = cooldown;
+    }
+	void Update ()
+    {
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        else
+        {
+            if (!countDownFinished)
+            {
+                if (this.gameObject.GetComponent<Destroy>() != null)
+                {
+                    countDownFinished = true;
+                    this.gameObject.GetComponent<Destroy>().DestroyObject();
+                }
+                else
+                {
+                    Debug.Log("This object does not have a destroy script");
+                }
+            }
+        }
+	}
+}
---
+++ ViveSandboxProj/Assets/Scripts/General Scripts/ObjectEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectEvent : MonoBehaviour
+{
+    public virtual void  StartAction(GameObject thisObj, GameObject otherObj)
+    {
+
+    }
+}

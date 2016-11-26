85727f909e9738c3b4858e02892ca8c0fde26436
Shady Shades <alexandermalusbohlin@gmail.com>
2016-11-26 09:47:56 +0100

    Fixed Level Manager so that it loads the level once. Fixed so that ObjectIntegration can choose to loop an event and also only run an event once.

--- ViveSandboxProj/Assets/Scripts/LevelManager.cs
+++ ViveSandboxProj/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour {
 
     [SerializeField] private string[] levels; //Write Down Maps in the Editor
     [SerializeField] private int levelIndex; //Index for the levels-array, set in the Editor.
+    [SerializeField] private bool levelLoaded = false;
 
     // Use this for initialization
     void Start()
@@ -17,16 +18,17 @@ public class LevelManager : MonoBehaviour {
     void Update()
     {
         //Change to a collision instead of button-press
-        if (Input.GetKeyDown(KeyCode.P))
+        if (!levelLoaded)
         {
+            levelLoaded = true;
             LoadNextLevel();
         }
     }
 
     public void LoadNextLevel()
     {
-        //levelIndex++;
-        //Debug.Log("yay next level");
-        //SceneManager.LoadScene("Scene_2");
+        levelIndex++;
+        Debug.Log("yay next level");
+        SceneManager.LoadScene(levels[levelIndex]);
     }
 }

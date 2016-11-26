7e60c622d27ef295438e4db04cfbcc1f0da16d33
Gustaf Wall <wall.gustaf@gmail.com>
2016-11-26 09:10:51 +0100

    Added script for respawning and spawning object

---
+++ ViveSandboxProj/Assets/Scripts/General Scripts/ObjRespawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjRespawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objToSpawn;
+    [Tooltip("Only fill out spawn pos OR distance to move, spawnPos = set position")]
+    [SerializeField] private Vector3 spawnPos;
+    [Tooltip("Only fill out target pos OR distance to move, distance = move X units forward")]
+    [SerializeField] private float distanceToMove;
+    [SerializeField] private float respawnTimer;
+
+    void Start()
+    {
+
+    }
+}

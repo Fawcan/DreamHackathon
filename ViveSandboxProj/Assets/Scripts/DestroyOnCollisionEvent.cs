9eb1fe90760f256b02f34fcf10835f22d20a1c37
glaenn <davidhalldin92@gmail.com>
2016-11-26 17:52:14 +0100

    Fixed respawn on the FUCKING basketball. Added som more prefabs!

---
+++ ViveSandboxProj/Assets/Scripts/DestroyOnCollisionEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnCollisionEvent : ObjectEvent
+{
+    public override void StartEvent(GameObject thisObj, GameObject otherObj)
+    {
+        base.StartEvent(thisObj, otherObj);
+        Destroy(otherObj);
+    }
+}

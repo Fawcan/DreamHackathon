ed8e93c40fd19e3d19177b74235cf0217201a89f
Gustaf Wall <wall.gustaf@gmail.com>
2016-11-26 18:27:36 +0100

    SPAWNOBJECT

---
+++ ViveSandboxProj/Assets/Scripts/SpawnObjectEvent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjectEvent : ObjectEvent
+{
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private Vector3 spawnPos;
+    [SerializeField] private bool useCooldown = false;
+    [SerializeField] private float spawnCooldown;
+    [SerializeField] private float timer;
+    [SerializeField] private bool spawnStarted = false;
+
+    public override void StartEvent(GameObject thisObj, GameObject otherObj)
+    {
+        base.StartEvent(thisObj, otherObj);
+
+        if(!spawnStarted)
+        {
+            spawnStarted = true;
+            StartSpawning();
+        }
+    }
+
+    void StartSpawning()
+    {
+        if(useCooldown)
+        {
+            timer = spawnCooldown;
+            useCooldown = false;
+        }
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if(timer <= 0)
+        {
+            Instantiate(objectToSpawn, spawnPos, transform.rotation);
+        }
+    }
+}

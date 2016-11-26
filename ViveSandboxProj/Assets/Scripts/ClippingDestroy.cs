61a52be5df7dbf59445d2e0b33da61c327091a58
UTB\frisal <albinfrisk@hotmail.com>
2016-11-26 09:38:37 +0100

    Min level

---
+++ ViveSandboxProj/Assets/Scripts/ClippingDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClippingDestroy : MonoBehaviour
+{
+    [SerializeField]
+    Collider ColliderBox;
+    [SerializeField]
+    GameObject Cube;
+
+	void OnCollisionEnter(Collision ColliderBox)
+    {
+        Destroy(Cube);
+    }
+}

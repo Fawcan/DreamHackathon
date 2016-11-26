9c94a008b503c28569773c90ceab606c7523b741
Shady Shades <alexandermalusbohlin@gmail.com>
2016-11-26 14:34:20 +0100

    Hopefully finished the Button Scripts as intended.

--- ViveSandboxProj/Assets/Scripts/FirstButtonPressedEvent.cs
+++ ViveSandboxProj/Assets/Scripts/FirstButtonPressedEvent.cs
@@ -3,26 +3,26 @@ using System.Collections;
 
 public class FirstButtonPressedEvent : ObjectEvent {
 
-    [SerializeField] private GameObject secondButton;
+    [SerializeField] private GameObject secondButton; //Store the second button in the Inspector
 
-    [SerializeField] public bool firstButtonPressable = true;
-    [SerializeField] public bool firstButtonPressed = false;
+    [SerializeField] public bool firstButtonPressable = true; //If the player can press the first button
+    [SerializeField] public bool firstButtonPressed = false; //If the player has pressed the first button
 
-    [SerializeField] public bool secondButtonPressable = false;
-    [SerializeField] public bool secondButtonPressed = false;
+    [SerializeField] public bool secondButtonPressable = false; //If the player can press the second button
+    [SerializeField] public bool secondButtonPressed = false; //If the player has pressed the second button
 
-    [SerializeField] public float timer;
-    [SerializeField] private float timerStartValue;
-    [SerializeField] private bool timerSet = false;
-    [SerializeField] public bool timerStopped = false;
+    [SerializeField] public float timer; //Timer for sequence
+    [SerializeField] private float timerStartValue; //The start value to tick down, set in the inspector
+    [SerializeField] public bool timerSet = false; //If the timer has been set
+    [SerializeField] public bool timerStopped = false; //If the timer has been stopped to check it's value.
 
-    [SerializeField] public bool sequenceComplete = false;
-    [SerializeField] public bool sequenceFailed = false;
+    [SerializeField] public bool sequenceComplete = false; //If the sequence is finished
+    [SerializeField] public bool sequenceFailed = false; //If the player failed to complete the sequence
 
     void Start()
     {
-        gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
-        secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.WHITE;
+        gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW; //Set the defualt color of the first button to yellow, indicating it can be pressed.
+        secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.WHITE; //Set the defualt color of the second button to white, indicating it cannot can be pressed.
     }
 
     public override void StartEvent(GameObject thisObj, GameObject otherObj)
@@ -30,30 +30,30 @@ public class FirstButtonPressedEvent : ObjectEvent {
         if (firstButtonPressable)
         {
             base.StartEvent(thisObj, otherObj);
-            firstButtonPressable = false;
-            firstButtonPressed = true;
+            firstButtonPressable = false; //At Event start, you cannot press the button again.
+            firstButtonPressed = true; //The button has been pressed
 
-            secondButtonPressable = true;
+            secondButtonPressable = true; //Make the second button pressable
 
-            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
-            secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN; //Change the color to green, inidcating it is finished.
+            secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.WHITE;
         }
     }
 
     void Update()
     {
-        if (firstButtonPressed && !timerSet)
+        if (firstButtonPressed && !timerSet) //If the button has been pressed and the timer hasn't been set.
         {
             timerSet = true;
-            timer = timerStartValue;
+            timer = timerStartValue; //Set the timer to it's starting value.
         }
 
-        if (timer > 0 && !timerStopped)
+        if (timer > 0 && !timerStopped) //Countdown as long as the timer hasn't been stopped.
         {
             timer -= Time.deltaTime;
         }
 
-        if (timer <= 0 && !sequenceComplete)
+        if (timer <= 0 && !sequenceComplete) //If the sequence wasn't completed or failed, set defualt values to flags.
         {
             firstButtonPressable = true;
             firstButtonPressed = false;
--- ViveSandboxProj/Assets/Scripts/SecondButtonPressedEvent.cs
+++ ViveSandboxProj/Assets/Scripts/SecondButtonPressedEvent.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class SecondButtonPressedEvent : ObjectEvent {
 
-    [SerializeField] private GameObject firstButton;
+    [SerializeField] private GameObject firstButton; //Store the first button in the Inspector
     [SerializeField] public FirstButtonPressedEvent firstButtonPressedEv;
 
-    [SerializeField] private float timerLow;
-    [SerializeField] private float timerMax;
+    [SerializeField] private float timerLow; //Set the low value for the timer interval
+    [SerializeField] private float timerMax; //Set the high value for the timer interval
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         firstButtonPressedEv = firstButton.GetComponent<FirstButtonPressedEvent>();
 	}
@@ -20,30 +20,56 @@ public class SecondButtonPressedEvent : ObjectEvent {
         if (firstButtonPressedEv.secondButtonPressable)
         {
             base.StartEvent(thisObj, otherObj);
-            firstButtonPressedEv.secondButtonPressed = true;
-            firstButtonPressedEv.secondButtonPressable = false;
-            firstButtonPressedEv.timerStopped = true;
+            firstButtonPressedEv.secondButtonPressed = true; //At Event start, you cannot press the button again.
+            firstButtonPressedEv.secondButtonPressable = false; //The button has been pressed
+            firstButtonPressedEv.timerStopped = true; //Stop the timer
         }
     }
 
     // Update is called once per frame
     void Update ()
     {
+        if (firstButtonPressedEv.timer <= timerMax && firstButtonPressedEv.timer >= timerLow) //While in between the intervals, make the button yellow.
+        {
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
+        }
+
+        //If the timer has been stopped by collision and timer being in between the interval, make the button green and complete the sequence.
         if (firstButtonPressedEv.timerStopped && firstButtonPressedEv.timer <= timerMax && firstButtonPressedEv.timer >= timerLow)
         {
             gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
             firstButtonPressedEv.sequenceComplete = true;
         }
-        else if (firstButtonPressedEv.timerStopped && firstButtonPressedEv.timer != 0 && firstButtonPressedEv.timer > timerMax && firstButtonPressedEv.timer < timerMax)
+        //If the timer has been stopped by collision and timer being outside the interval, make all the buttons red and fail the sequence.
+        else if (firstButtonPressedEv.timerStopped && firstButtonPressedEv.timerSet && firstButtonPressedEv.timer > timerMax || firstButtonPressedEv.timer < timerMax)
         {
             firstButtonPressedEv.sequenceFailed = true;
         }
 
+        //If sequence wasn't completed, reset all the values to defualt.
+        if (firstButtonPressedEv.timerSet && firstButtonPressedEv.timer <= 0)
+        {
+            firstButtonPressedEv.timer = 0;
+            firstButtonPressedEv.timerSet = false;
+            firstButtonPressedEv.timerStopped = false;
+
+            firstButtonPressedEv.secondButtonPressed = false;
+            firstButtonPressedEv.secondButtonPressable = false;
+            firstButtonPressedEv.firstButtonPressable = true;
+            firstButtonPressedEv.firstButtonPressed = false;
+
+            firstButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.WHITE;
+        }
+
+        //If sequence failed, reset all the values to defualt and set the color of the buttons to red.
         if (firstButtonPressedEv.sequenceFailed)
         {
             firstButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.RED;
             gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.RED;
 
+            firstButtonPressedEv.timer = 0;
+            firstButtonPressedEv.timerSet = false;
             firstButtonPressedEv.timerStopped = false;
 
             firstButtonPressedEv.secondButtonPressed = true;

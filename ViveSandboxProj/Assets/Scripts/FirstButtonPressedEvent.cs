9e9bbdb8161ddcfcd72680ce1800608f805ca863
Shady Shades <alexandermalusbohlin@gmail.com>
2016-11-26 13:18:01 +0100

    Integrated VR Controls with Button Sequence Scripts

---
+++ ViveSandboxProj/Assets/Scripts/FirstButtonPressedEvent.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstButtonPressedEvent : ObjectEvent {
+
+    [SerializeField] private GameObject secondButton;
+
+    [SerializeField] public bool firstButtonPressable = true;
+    [SerializeField] public bool firstButtonPressed = false;
+
+    [SerializeField] public bool secondButtonPressable = false;
+    [SerializeField] public bool secondButtonPressed = false;
+
+    [SerializeField] public float timer;
+    [SerializeField] private float timerStartValue;
+    [SerializeField] private bool timerSet = false;
+
+    [SerializeField] public bool sequenceComplete = false;
+    [SerializeField] public bool sequenceFailed = false;
+
+    void Start()
+    {
+        gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
+        secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.WHITE;
+    }
+
+    public override void StartEvent(GameObject thisObj, GameObject otherObj)
+    {
+        if (firstButtonPressable)
+        {
+            base.StartEvent(thisObj, otherObj);
+            firstButtonPressable = false;
+            firstButtonPressed = true;
+
+            secondButtonPressable = true;
+
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
+            secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.YELLOW;
+        }
+    }
+
+    void Update()
+    {
+        if (firstButtonPressed && !timerSet)
+        {
+            timerSet = false;
+            timer = timerStartValue;
+        }
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0 && !sequenceComplete)
+        {
+            firstButtonPressable = true;
+            firstButtonPressed = false;
+            secondButtonPressable = false;
+            secondButtonPressed = false;
+        }
+
+        if (sequenceComplete) //Call another event since the puzzle is solved.
+        {
+            timer = 0;
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
+            secondButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
+        }
+    }
+}
---
+++ ViveSandboxProj/Assets/Scripts/SecondButtonPressedEvent.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondButtonPressedEvent : ObjectEvent {
+
+    [SerializeField] private GameObject firstButton;
+    [SerializeField] public FirstButtonPressedEvent firstButtonPressedEv;
+
+    [SerializeField] private float timerLow;
+    [SerializeField] private float timerMax;
+
+	// Use this for initialization
+	void Start ()
+    {
+        firstButtonPressedEv = firstButton.GetComponent<FirstButtonPressedEvent>();
+	}
+
+    public override void StartEvent(GameObject thisObj, GameObject otherObj)
+    {
+        if (firstButtonPressedEv.secondButtonPressable)
+        {
+            base.StartEvent(thisObj, otherObj);
+            firstButtonPressedEv.secondButtonPressed = true;
+            firstButtonPressedEv.secondButtonPressable = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (firstButtonPressedEv.timer <= timerMax && firstButtonPressedEv.timer >= timerLow)
+        {
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.GREEN;
+            firstButtonPressedEv.sequenceComplete = true;
+        }
+        else if (firstButtonPressedEv.timer != 0 && firstButtonPressedEv.timer > timerMax && firstButtonPressedEv.timer < timerMax)
+        {
+            firstButtonPressedEv.sequenceFailed = true;
+        }
+
+        if (firstButtonPressedEv.sequenceFailed)
+        {
+            firstButton.GetComponent<ColorManager>().NewColor = ColorManager.Colors.RED;
+            gameObject.GetComponent<ColorManager>().NewColor = ColorManager.Colors.RED;
+        }
+    }
+}
